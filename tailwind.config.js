4d945e8d9304e8576f964de44b786d047c157d2c
Steve Gifford <sjg@mousebirdconsulting.com>
2021-02-19 18:02:29 -0800

    Need to include the Javascript that pulls in some Tailwind classes for evaluation in the purge of the main css.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   purge: {
-    content: ['./src/**/*.njk', './src/**/*.svg'],
+    content: ['./src/**/*.njk', './src/**/*.svg','./src/**/*.js','.eleventy.js'],
     layers: ['components', 'utilities'],
     options: {
       safelist: ['header-shadow', '-translate-y-full', 'hidden']
