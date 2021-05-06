92f72c0cd76a8bff5ba9b8281f8ea0e39aae5597
Steve Gifford <sjg@mousebirdconsulting.com>
2021-05-06 16:33:02 -0700

    Pulldown menu working for mobile.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,13 +26,28 @@ Toggle the menu when pressed on hamburger button
 Only on mobile devices
 */
 
-const menu = document.getElementById('menu');
-const searchBox = document.getElementById('search');
+var menu_status = false;
+
+const menu_horiz = document.getElementById('menu-horiz');
+const menu_vert = document.getElementById('menu-vert');
 const menuToggle = document.getElementById('menu-toggle');
+const menu_hamburger = document.getElementById('menu-hamburger');
+const menu_x = document.getElementById('menu-x');
 
 menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-  searchBox.classList.toggle('hidden');
+  if (menu_status) {
+    menu_status = false;
+    menu_horiz.classList.remove('hidden');
+    menu_vert.classList.add('hidden');
+    menu_hamburger.classList.remove('hidden')
+    menu_x.classList.add('hidden')
+  } else {
+    menu_status = true;
+    menu_horiz.classList.add('hidden');
+    menu_vert.classList.remove('hidden');
+    menu_hamburger.classList.add('hidden')
+    menu_x.classList.remove('hidden')
+  }
 }, false);
 
 /*
